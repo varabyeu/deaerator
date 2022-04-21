165f09eab46f0c2b2ca8cf9299a228f3c141e3dc
Varabyeu <varabyeu.v@yandex.by>
2022-04-21 11:49:33 +0300

    Initial commit once again

--- deaerator/tank/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-
-from .models import InitialData
-
-admin.site.register(InitialData)
-# Register your models here.
--- deaerator/tower/apps.py
+++ deaerator/tower/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class TankConfig(AppConfig):
+class TowerConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'tank'
+    name = 'tower'
